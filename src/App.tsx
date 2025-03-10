d01115e29670aa955c84786c31c22c7271980e75
DustyFlower <lukashevanatalia1992@gmail.com>
2025-03-10 16:20:26 +0100

    filter and map

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+    const names = ['Dimych', 'Sveta', 'Katya', 'Viktor', 'Ignat']
+    const users = [{id: 1, name: 'Dimych'}, {id: 2, name: 'Sveta'}, {id: 3, name: 'Katya'}, {
+        id: 4,
+        name: 'Viktor'
+    }, {id: 5, name: 'Ignat'}]
+    const liElements = users.map(n => <li key={n.id}>{n.id} - {n.name}</li>)
+
+    return (
+        <div className="App">
+            <ul>
+                {liElements}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
