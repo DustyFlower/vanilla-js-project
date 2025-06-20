e5a26b522de745591b0367a2466f67dddd1194f4
DustyFlower <lukashevanatalia1992@gmail.com>
2025-06-20 16:50:43 +0200

    Associative arrays

---
+++ src/08/08_01.ts
@@ -0,0 +1,29 @@
+export const usersObj = {
+    '0': 'Dymich',
+    '1': 'Natasha',
+    '2': 'Valera',
+    '3': 'Katya'
+}
+
+
+
+//users[1]
+
+let user = {id: 100500, name: 'Igor'}
+
+//users[user.id] = user;
+//delete users[user.id]
+//users[user.id].name = 'Vitya'
+
+export const usersArray = [
+    {id: 101, name: 'Dimych'},
+    {id: 3232312, name: 'Natasha'},
+    {id: 1212, name: 'Valera'},
+    {id: 1, name: 'Katya'}
+]
+
+//usersArray.find(u => u.id === 1)
+
+//let usersCopy = [...usersArray.filter(),user]
+
+//let usersArray = usersArray.filter(u => u.id !==user.id)
